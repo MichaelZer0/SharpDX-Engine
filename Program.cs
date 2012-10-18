a0e53bc6cf0f105ae667702bcc2dcf55b19e6210
NekuSoul <GitHub@NekuSoul.de.vu>
2012-10-18 19:20:40 +0200

    First Tests

---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿using SharpDX;
+using SharpDX.Direct2D1;
+using SharpDX.Direct3D10;
+using SharpDX.DXGI;
+using SharpDX.Windows;
+using System;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using AlphaMode = SharpDX.Direct2D1.AlphaMode;
+using Device1 = SharpDX.Direct3D10.Device1;
+using DriverType = SharpDX.Direct3D10.DriverType;
+using Factory = SharpDX.DXGI.Factory;
+using FeatureLevel = SharpDX.Direct3D10.FeatureLevel;
+
+namespace NekuSoul.SharpDX_Engine
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            var form = new RenderForm();
+
+            // SwapChain description
+            var desc = new SwapChainDescription()
+            {
+                BufferCount = 1,
+                ModeDescription =
+                    new ModeDescription(form.ClientSize.Width, form.ClientSize.Height,
+                                        new Rational(60, 1), Format.R8G8B8A8_UNorm),
+                IsWindowed = true,
+                OutputHandle = form.Handle,
+                SampleDescription = new SampleDescription(1, 0),
+                SwapEffect = SwapEffect.Discard,
+                Usage = Usage.RenderTargetOutput
+            };
+
+            // Create Device and SwapChain
+            Device1 device;
+            SwapChain swapChain;
+            Device1.CreateWithSwapChain(DriverType.Hardware, DeviceCreationFlags.BgraSupport, desc, FeatureLevel.Level_10_0, out device, out swapChain);
+
+            var d2dFactory = new SharpDX.Direct2D1.Factory();
+
+            int width = form.ClientSize.Width;
+            int height = form.ClientSize.Height;
+
+            var rectangleGeometry = new RectangleGeometry(d2dFactory, new RectangleF(0f, 0f, 100f, 100f));
+
+            // Ignore all windows events
+            Factory factory = swapChain.GetParent<Factory>();
+            factory.MakeWindowAssociation(form.Handle, WindowAssociationFlags.IgnoreAll);
+
+            // New RenderTargetView from the backbuffer
+            Texture2D backBuffer = Texture2D.FromSwapChain<Texture2D>(swapChain, 0);
+            var renderView = new RenderTargetView(device, backBuffer);
+
+            Surface surface = backBuffer.QueryInterface<Surface>();
+
+            var d2dRenderTarget = new RenderTarget(d2dFactory, surface,
+                                                            new RenderTargetProperties(new SharpDX.Direct2D1.PixelFormat(Format.Unknown, AlphaMode.Premultiplied)));
+            Bitmap bitmap = LoadFromFile(d2dRenderTarget, "Smiley.bmp");
+
+            var solidColorBrush = new BitmapBrush(d2dRenderTarget, LoadFromFile(d2dRenderTarget, "Smiley.bmp"));
+            //solidColorBrush.ExtendModeX = ExtendMode.Wrap;
+            //solidColorBrush.Transform = new Matrix3x2(0f,0f,100f,100f,200f,200f);
+            //solidColorBrush.ExtendModeY = ExtendMode.Wrap;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // Main loop
+            RenderLoop.Run(form, () =>
+            {
+                d2dRenderTarget.BeginDraw();
+                d2dRenderTarget.Clear(Color.Black);
+                //solidColorBrush.Color = new Color4(1, 1, 1, (float)Math.Abs(Math.Cos(stopwatch.ElapsedMilliseconds * .001)));
+                d2dRenderTarget.FillGeometry(rectangleGeometry, solidColorBrush, null);
+                d2dRenderTarget.EndDraw();
+
+                swapChain.Present(0, PresentFlags.None);
+            });
+
+            // Release all resources
+            renderView.Dispose();
+            backBuffer.Dispose();
+            device.ClearState();
+            device.Flush();
+            device.Dispose();
+            device.Dispose();
+            swapChain.Dispose();
+            factory.Dispose();
+        }
+
+        public static Bitmap LoadFromFile(RenderTarget renderTarget, string file)
+        {
+            // Loads from file using System.Drawing.Image
+            using (var bitmap = (System.Drawing.Bitmap)System.Drawing.Image.FromFile(file))
+            {
+                var sourceArea = new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height);
+                var bitmapProperties = new BitmapProperties(new SharpDX.Direct2D1.PixelFormat(Format.R8G8B8A8_UNorm, AlphaMode.Premultiplied));
+                var size = new DrawingSize(bitmap.Width, bitmap.Height);
+
+                // Transform pixels from BGRA to RGBA
+                int stride = bitmap.Width * sizeof(int);
+                using (var tempStream = new DataStream(bitmap.Height * stride, true, true))
+                {
+                    // Lock System.Drawing.Bitmap
+                    var bitmapData = bitmap.LockBits(sourceArea, ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+
+                    // Convert all pixels 
+                    for (int y = 0; y < bitmap.Height; y++)
+                    {
+                        int offset = bitmapData.Stride * y;
+                        for (int x = 0; x < bitmap.Width; x++)
+                        {
+                            // Not optimized 
+                            byte B = Marshal.ReadByte(bitmapData.Scan0, offset++);
+                            byte G = Marshal.ReadByte(bitmapData.Scan0, offset++);
+                            byte R = Marshal.ReadByte(bitmapData.Scan0, offset++);
+                            byte A = Marshal.ReadByte(bitmapData.Scan0, offset++);
+                            int rgba = R | (G << 8) | (B << 16) | (A << 24);
+                            tempStream.Write(rgba);
+                        }
+
+                    }
+                    bitmap.UnlockBits(bitmapData);
+                    tempStream.Position = 0;
+
+                    return new Bitmap(renderTarget, size, tempStream, stride, bitmapProperties);
+                }
+            }
+        }
+    }
+}
