740daebfa29daaffcae5de9d6fe6f6bd9134bfd7
NekuSoul <GitHub@NekuSoul.de.vu>
2012-10-22 15:05:28 +0200

    Began Work

---
+++ Objects/DrawableObject.cs
@@ -0,0 +1,21 @@
+﻿using SharpDX.Direct2D1;
+
+namespace NekuSoul.SharpDX_Engine.Objects
+{
+    public class Coordinates
+    {
+        float X = 0f;
+        float Y = 0f;
+    }
+
+    public class DrawableObject
+    {
+        Bitmap Texture;
+        Coordinates Position;
+
+        public DrawableObject(RenderTarget RT)
+        {
+            Texture = Utitities.Converter.LoadFromFile(RT, "Smiley.bmp");
+        }
+    }
+}
