07a8757b1ee3652144a1d8750e36d55955fab507
NekuSoul <GitHub@NekuSoul.de.vu>
2012-10-24 17:08:35 +0200

    Added Mouse-Class

---
+++ Input/Mouse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.DirectInput;
+
+namespace NekuSoul.SharpDX_Engine.Input
+{
+    class Mouse
+    {
+        
+    }
+}
