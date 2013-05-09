3a42e6981348850c8a7cdc8a5429e0180c61364d
NekuSoul <Github@NekuSoul.de.vu>
2013-05-09 14:34:22 +0200

    Added MainMenu

--- SharpDX-Engine-Tutorial/Scenes/MainMenu.cs
+++ SharpDX-Engine-Tutorial/Scenes/MainMenu.cs
@@ -5,10 +5,19 @@ namespace NekuSoul.SharpDX_Engine_Tutorial.Scenes
 {
     class MainMenu : SharpDX_Engine.Scene
     {
-        public override void Update()
+        //! The Scene is built here
+        public MainMenu()
         {
+            
         }
 
+        //! This method is called 60 times per second and contains the logic for the scene.
+        public override void Update()
+        {
+
+        }
+
+        //! This method is called to draw the Scene and returns all objects that should get drawn.
         public override List<DrawableObject> Draw()
         {
             return new List<DrawableObject>();
